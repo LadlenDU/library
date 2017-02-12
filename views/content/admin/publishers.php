5eb0f7703e7441537bb293a4d44f483693ba89e2
Dolgov Dmitriy <d.dolgov@i2013.ru>
2017-02-12 21:14:02 +0300

    издательства

--- views/content/admin/publishers.php
+++ views/content/admin/publishers.php
@@ -3,7 +3,7 @@
 /* @var $this app\core\View */
 /* @var $values app\core\Container */
 
-#use app\widgets\Form;
+use app\widgets\Form;
 use app\helpers\Html;
 
 $this->title = Html::createTitle('список издательств');
@@ -11,45 +11,45 @@ $this->title = Html::createTitle('список издательств');
 ?>
 <script type="text/javascript">
     // <![CDATA[
-    $(document).ready(function () {
-        var dtTblInfo = app.dataTableInfo;
+    $(function () {
+        var dtTblInfo = jQuery.extend(true, {}, app.dataTableInfo);
         dtTblInfo.columnDefs = [
             {orderable: false, targets: -1}
         ];
+        dtTblInfo.bFilter = false;
         $('#publisher_list').DataTable(dtTblInfo);
     });
     // ]]>
 </script>
 <h2>Издательства</h2>
-<table id="publisher_list" class="table table-striped table-bordered" width="100%" cellspacing="0">
+<?php Form::startForm(['action' => '/admin/']) ?>
+<table id="publisher_list" class="table table-striped table-bordered">
     <thead>
     <tr>
         <th>Название</th>
         <th>&nbsp;</th>
     </tr>
     </thead>
+    <tbody>
+    <?php foreach ($values->items as $item): ?>
+    <tr>
+        <td><?php Html::h($item->name) ?></td>
+        <td>
+            <button type="submit" class="btn btn-default btn-sm" data-id="<?php $item->id ?>" data-action="edit">
+                <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>&nbsp;Редактировать
+            </button>
+            <button type="submit" class="btn btn-default btn-sm" data-id="<?php $item->id ?>" data-action="remove">
+                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>&nbsp;Удалить
+            </button>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+    </tbody>
     <tfoot>
     <tr>
         <th>Название</th>
         <th>&nbsp;</th>
     </tr>
     </tfoot>
-    <tbody>
-    <!--<tr>
-        <td>Tiger Nixon</td>
-        <td>System Architect</td>
-        <td>Edinburgh</td>
-        <td>61</td>
-        <td>2011/04/25</td>
-        <td>$320,800</td>
-    </tr>
-    <tr>
-        <td>Garrett Winters</td>
-        <td>Accountant</td>
-        <td>Tokyo</td>
-        <td>63</td>
-        <td>2011/07/25</td>
-        <td>$170,750</td>
-    </tr>-->
-    </tbody>
 </table>
+<?php Form::endForm() ?>
